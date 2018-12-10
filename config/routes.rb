51058131cac3581093eff2551a23ae3a554fc41e
Andrew Haines <andrew@fiatinsight.com>
2018-12-10 12:58:40 -0500

    Included routes for messages, comments, and attachments

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ FiatPublication::Engine.routes.draw do
   resources :articles
   resources :content_blocks
   resources :authors
+  resources :messages
+  resources :comments
+  resources :attachments
 end
