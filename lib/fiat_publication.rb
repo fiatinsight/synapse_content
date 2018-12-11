8803df0fbca83d83ca03bec9bf25040d2c3ca145
Andrew Haines <andrew@fiatinsight.com>
2018-12-11 14:00:56 -0500

    Bugfix

--- lib/fiat_publication.rb
+++ lib/fiat_publication.rb
@@ -2,4 +2,6 @@ require "fiat_publication/engine"
 
 module FiatPublication
   mattr_accessor :new_message_redirect_path
+  mattr_accessor :new_page_redirect_path
+  mattr_accessor :new_article_redirect_path
 end
