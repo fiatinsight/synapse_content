49de118b939bf393fee08bedc28331c8e717f944
Andrew Haines <andrew@fiatinsight.com>
2018-12-11 09:28:06 -0500

    Bugfix

--- app/controllers/fiat_publication/messages_controller.rb
+++ app/controllers/fiat_publication/messages_controller.rb
@@ -16,7 +16,7 @@ module FiatPublication
 
       respond_to do |format|
         if @message.save
-          format.html { redirect_to main_app.url_for(@message), notice: 'Message was created.' }
+          format.html { redirect_to main_app.message_path(@message.id), notice: 'Message was created.' }
         else
           format.html { render action: "new" }
         end
