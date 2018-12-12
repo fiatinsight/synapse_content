e6cd25db85610e9e1c27a644184674f980d3510c
Andrew Haines <andrew@fiatinsight.com>
2018-12-11 19:35:47 -0500

    Working on page form

--- app/models/fiat_publication/page.rb
+++ app/models/fiat_publication/page.rb
@@ -5,7 +5,7 @@ module FiatPublication
     self.table_name = "fi_pages"
 
     belongs_to :publisher, polymorphic: true
-    has_many :content_blocks, as: :publishable
+    has_many :content_blocks, as: :publishable, class_name: 'FiatPublication::ContentBlock'
 
     has_one_attached :image
 
