d414988834381ae874b16f453ad37af148a46680
Andrew Haines <andrew@fiatinsight.com>
2018-12-17 08:06:38 -0500

    Updated page edit form

--- lib/fiat_publication.rb
+++ lib/fiat_publication.rb
@@ -6,6 +6,7 @@ module FiatPublication
 
   mattr_accessor :new_message_redirect_path
   mattr_accessor :new_page_path
+  mattr_accessor :view_page_path
   mattr_accessor :new_page_redirect_path
   mattr_accessor :new_article_redirect_path
 end
