a1dd7a952d9225c7d4abed3128490a856afc4931
Andrew Haines <andrew@fiatinsight.com>
2019-02-10 08:21:54 -0500

    Added resources for articles (incomplete)

---
+++ app/controllers/fiat_publication/articles_controller.rb
@@ -0,0 +1,63 @@
+module FiatPublication
+  class ArticlesController < ActionController::Base
+    # include ActionView::Helpers::TextHelper
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+    def index
+      # @articles = Current.publisher.articles.order("title ASC").all
+      @articles = Article.all
+    end
+
+    def new
+      @article = Article.new
+    end
+
+    def create
+      @article = Article.create(article_params)
+
+      respond_to do |format|
+        if @article.save
+          format.html { redirect_to main_app.send(FiatPublication.new_article_redirect_path, @article), notice: 'Article was created.' }
+        else
+          format.html { render action: "new" }
+        end
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      respond_to do |format|
+        if @article.update_attributes(article_params)
+          format.html { redirect_back(fallback_location: article_path(@article), notice: 'Article successfully updated.') }
+        else
+          format.html { render action: "edit" }
+        end
+      end
+    end
+
+    def preview
+      @article = Article.find(params[:id])
+    end
+
+    def destroy
+      @article.destroy
+
+      respond_to do |format|
+        format.html { redirect_to account_publisher_articles_path(@article.publisher), notice: 'Article was successfully deleted.' }
+      end
+    end
+
+    private
+
+      def set_article
+        @article = Article.find(params[:id])
+      end
+
+      def article_params
+        params.require(:article).permit(:publisher_type, :publisher_id, :title, :slug, :image, :excerpt, :image_placement)
+      end
+
+  end
+end
--- app/models/fiat_publication/article.rb
+++ app/models/fiat_publication/article.rb
@@ -1,8 +1,8 @@
 module FiatPublication
   class Article < ApplicationRecord
     include Tokenable
-    audited
-    has_associated_audits
+    # audited
+    # has_associated_audits
 
     self.table_name = "fi_articles"
 
@@ -13,5 +13,16 @@ module FiatPublication
     accepts_nested_attributes_for :custom_fields, reject_if: :all_blank, allow_destroy: true
     has_many :content_label_assignments, as: :assignable, dependent: :destroy
     has_many :content_labels, through: :content_label_assignments
+
+    has_one_attached :image
+
+    validates :title, presence: true
+
+    enum image_placement: {
+      billboard: 0,
+      right: 1
+    }, _prefix: :image
+
+    scope :published, lambda { where.not(published_at: nil).includes(:author) }
   end
 end
