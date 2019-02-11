63c6ecf135bd4db5205bf5f3429d95bad074cec8
Andrew Haines <andrew@fiatinsight.com>
2019-02-11 04:44:13 -0500

    Building attachments out

--- lib/fiat_publication.rb
+++ lib/fiat_publication.rb
@@ -4,12 +4,13 @@ module FiatPublication
   # mattr_accessor :publisher_type
   # mattr_accessor :publisher_id
 
-  mattr_accessor :new_message_redirect_path
   mattr_accessor :new_page_path
   mattr_accessor :view_page_path
-  mattr_accessor :new_page_redirect_path
   mattr_accessor :new_article_path
   mattr_accessor :view_article_path
   mattr_accessor :new_article_redirect_path
   mattr_accessor :new_content_block_redirect_path
+  mattr_accessor :new_message_redirect_path
+  mattr_accessor :new_attachment_redirect_path
+  mattr_accessor :new_page_redirect_path
 end
