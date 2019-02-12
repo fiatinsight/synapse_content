f3dc037b3a2be60e9bae176ca6b6c7af8488e271
Andrew Haines <andrew@fiatinsight.com>
2019-02-12 07:45:58 -0500

    Added forms for nav items

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ FiatPublication::Engine.routes.draw do
   resources :messages
   resources :comments
   resources :attachments
+  resources :navigation_groups
+  resources :navigation_items
   resources :content_labels
   resources :content_label_assignments
   resources :custom_fields
